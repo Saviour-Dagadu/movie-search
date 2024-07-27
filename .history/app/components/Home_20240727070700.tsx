914aeef6c9f89e971d2d589af5a0e09640058482
Saviour-Dagadu <savicane@gmail.com>
2024-07-27 07:19:20 +0000

    Adding changes ,ade to Home.tsx

--- .history/app/components/Home_20240727070700.tsx
+++ .history/app/components/Home_20240727070700.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Loading from '../Loading';
 import { useSearchParams } from 'next/navigation';
 import axios from 'axios';
@@ -12,22 +12,22 @@ import dynamic from 'next/dynamic';
 
 const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
-interface IMovie {
-    poster_path: string;
-    title: string;
-    genres: {
-        name: string;
-        id: string;
-    }[];
-    original_language: string;
-    release_date: string;
-    runtime: string;
-    vote_average: string;
-    overview: string;
-    videos: { results: { type: string; key: string }[] };
-}
+const Home = () => {
+    interface IMovie {
+        poster_path: string;
+        title: string;
+        genres: {
+            name: string;
+            id: string;
+        }[];
+        original_language: string;
+        release_date: string;
+        runtime: string;
+        vote_average: string;
+        overview: string;
+        videos: { results: { type: string; key: string }[] };
+    }
 
-const MovieComponent = () => {
     const searchParams = useSearchParams();
 
     const [isLoading, setIsLoading] = useState(false);
@@ -88,8 +88,9 @@ const MovieComponent = () => {
     }, [movie]);
 
     return (
-        <div>
+        <div className='bg-secondary relative px-4 md:px-0'>
             {isLoading && <Loading />}
+
             <div className='container mx-auto min-h-[calc(100vh-77px)] flex items-center relative'>
                 <div className='flex flex-col lg:flex-row gap-10 lg:mx-10 py-20'>
                     <div className='mx-auto flex-none relative'>
@@ -116,7 +117,7 @@ const MovieComponent = () => {
                                 <Genres
                                     key={genre.id}
                                     index={index}
-                                    lenght={movie.genres.length}
+                                    lenght={movie.genres.length} 
                                     name={genre.name}
                                 />
                             ))}
@@ -170,14 +171,6 @@ const MovieComponent = () => {
                 )}
             </div>
         </div>
-    );
-};
-
-const Home = () => {
-    return (
-        <Suspense fallback={<Loading />}>
-            <MovieComponent />
-        </Suspense>
     );
 };
 
--- .history/app/components/Home_20240727071115.tsx
+++ .history/app/components/Home_20240727071115.tsx
@@ -12,21 +12,6 @@ import dynamic from 'next/dynamic';
 
 const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
-interface IMovie {
-    poster_path: string;
-    title: string;
-    genres: {
-        name: string;
-        id: string;
-    }[];
-    original_language: string;
-    release_date: string;
-    runtime: string;
-    vote_average: string;
-    overview: string;
-    videos: { results: { type: string; key: string }[] };
-}
-
 const MovieComponent = () => {
     const searchParams = useSearchParams();
 
